2b36c6068dccfd9228e956fe374f7bfa73db7807
yuehaoa <53177683+yuehaoa@users.noreply.github.com>
2020-07-19 14:56:04 +0800

    增加导弹发射效果

--- Assets/Scripts/OrdinaryCannon.cs
+++ Assets/Scripts/OrdinaryCannon.cs
@@ -5,31 +5,37 @@ using UnityEngine.EventSystems;
 
 public class OrdinaryCannon : MonoBehaviour
 {
-    public GameObject bullet;
-    private Vector3 direction = new Vector3(1, 0, 0);
-    // Start is called before the first frame update
-    void Start()
-    {
-        
+     public GameObject bullet;
+     private Vector3 direction = new Vector3(1, 0, 0);
+     // Start is called before the first frame update
+     void Start()
+     {
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-        if(Input.GetMouseButtonDown(0))
-        {
-            Debug.Log("123");
-            if (!EventSystem.current.IsPointerOverGameObject())
-            {
-                Fire();
-            }
-        }
-    }
+     // Update is called once per frame
+     void Update()
+     {
 
-    public void Fire()
-    {
-        GameObject gameObject = Instantiate(bullet, transform.position+ direction, Quaternion.identity);
-        gameObject.GetComponent<OrdinaryBullet>();
+         if(Input.GetMouseButtonDown(0))
+         {
+             Debug.Log("123");
+             if (!EventSystem.current.IsPointerOverGameObject())
+             {
+                 Fire();
+             }
+         }
+     }
+
+     public void Fire()
+     {
+        Vector3 mousePosition = Input.mousePosition;
+        mousePosition = Camera.main.ScreenToWorldPoint(mousePosition);
+        // 因为是2D
+        mousePosition.z = 0;
+        // 子弹角度
+        float fireAngle = Vector2.Angle(mousePosition - this.transform.position, Vector2.up);
+        GameObject gameObject = Instantiate(bullet, transform.position, Quaternion.identity);
+        gameObject.GetComponent<Rigidbody>().velocity = ((mousePosition - transform.position).normalized * 30);
+        gameObject.transform.eulerAngles = new Vector3(0, 0, fireAngle);
     }
 }
