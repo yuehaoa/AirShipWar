6d81e44a15cfe6c2b9abb5539ec16c9c3ad8701a
liangyingshao <1669807584@qq.com>
2020-07-21 01:55:29 +0800

    基本框架

--- Assets/Scripts/OrdinaryBullet.cs
+++ Assets/Scripts/OrdinaryBullet.cs
@@ -4,17 +4,36 @@ using UnityEngine;
 
 public class OrdinaryBullet : Bullet
 {
-    
+    public GameObject explosionEffectPrefab;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    void OnBecameInvisible()
+    {
+        if(gameObject)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    void OnCollisionEnter(Collision col)
+    {
+        GameObject effect = GameObject.Instantiate(explosionEffectPrefab, transform.position, transform.rotation);
+        Destroy(effect, 1);
+        col.gameObject.GetComponent<shipComponent>().damage(10);
+        if (gameObject)
+        {
+            Destroy(gameObject);
+        }
     }
 }
